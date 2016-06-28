4d690e724d9a6491a2b62354355df77e485bf9e2
cxsummer <colin.x.summer@nasa.gov>
2016-06-28 05:57:04 -0700

    blah

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 
-
-for dir in src/hyperloop/Python/*; do
-	echo "$dir"
+REGEX='*.py'
+for x in src/hyperloop/Python/*; do
+	if [ -f $x ] && [[ $x =~ .*\.py$ ]] && ! [[ $x =~ ^__ ]]; then
+		echo $x
+	fi
 done
