bdb6f8da6cf41cc928f4403857ad9ebd4433e34c
cxsummer <colin.x.summer@nasa.gov>
2016-06-28 06:47:15 -0700

    finished update script

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
+source activate tools
 
 REGEX='*.py'
-for x in src/hyperloop/Python/*; do
-	if [ -f $x ] && [[ $x =~ .*\.py$ ]] && ! [[ $x =~ ^__ ]]; then
-		echo $x
-	fi
+DIR="src/hyperloop/Python src/hyperloop/Python/tests"
+for y in $DIR; do
+	for x in $y/*; do
+		if [ -f $x ] && [[ $x =~ .*\.py$ ]] && ! [[ $x =~ .*__init__.py$ ]]; then
+			echo $x
+			git checkout master $x
+			yapf -i $x
+		fi
+	done
 done
+
